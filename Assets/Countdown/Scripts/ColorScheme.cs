3d2cdd7ee56f853dd195e42d653a1126231f921b
Cooper Saye <coopersaye@gmail.com>
2020-09-25 16:01:35 -0700

    Implement color scheme switching.

--- Assets/Countdown/Scripts/ColorScheme.cs
+++ Assets/Countdown/Scripts/ColorScheme.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Countdown
 {
@@ -8,10 +11,17 @@ namespace Countdown
         [SerializeField] private ColorSchemeScriptable lightMode = null;
         [SerializeField] private ColorSchemeScriptable darkMode = null;
 
-        // [Header("Max Color References")]
-        // [Header("Mid Color References")]
-        // [Header("Min Color References")]
-        // [Header("Special Color References")]
+        [Header("References")]
+        [SerializeField] private List<Image> maxColorImages = new List<Image>();
+        [SerializeField] private List<TextMeshProUGUI> maxColorTexts = new List<TextMeshProUGUI>();
+        [SerializeField] private List<Image> modColorImages = new List<Image>();
+        [SerializeField] private List<TextMeshProUGUI> modColorTexts = new List<TextMeshProUGUI>();
+        [SerializeField] private List<Image> midColorImages = new List<Image>();
+        [SerializeField] private List<TextMeshProUGUI> midColorTexts = new List<TextMeshProUGUI>();
+        [SerializeField] private List<Image> minColorImages = new List<Image>();
+        [SerializeField] private List<TextMeshProUGUI> minColorTexts = new List<TextMeshProUGUI>();
+        [SerializeField] private List<Image> specialColorImages = new List<Image>();
+        [SerializeField] private List<TextMeshProUGUI> specialColorTexts = new List<TextMeshProUGUI>();
 
         public static Color maxColor, modColor, midColor, minColor, specialColor;
 
@@ -56,26 +66,36 @@ namespace Countdown
         private void SetMaxColor(Color color)
         {
             maxColor = color;
+            foreach (Image image in maxColorImages) image.color = color;
+            foreach (TextMeshProUGUI text in maxColorTexts) text.color = color;
         }
 
         private void SetModColor(Color color)
         {
             modColor = color;
+            foreach (Image image in modColorImages) image.color = color;
+            foreach (TextMeshProUGUI text in modColorTexts) text.color = color;
         }
 
         private void SetMidColor(Color color)
         {
             midColor = color;
+            foreach (Image image in midColorImages) image.color = color;
+            foreach (TextMeshProUGUI text in midColorTexts) text.color = color;
         }
 
         private void SetMinColor(Color color)
         {
             minColor = color;
+            foreach (Image image in minColorImages) image.color = color;
+            foreach (TextMeshProUGUI text in minColorTexts) text.color = color;
         }
 
         private void SetSpecialColor(Color color)
         {
             specialColor = color;
+            foreach (Image image in specialColorImages) image.color = color;
+            foreach (TextMeshProUGUI text in specialColorTexts) text.color = color;
         }
     }
 }
